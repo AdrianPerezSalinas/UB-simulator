338f9fdfa440058cf7dd05849f36a621e481d172
Adrián Pérez Salinas <adrian.perez@bsc.es>
2020-02-13 17:48:49 +0100

    trials

--- ub_simulator/__init__.py
+++ ub_simulator/__init__.py
@@ -1,2 +1,6 @@
 
+import numpy_ub
+import qupy_ub
+import qutip_ub
+import tensornetwork_ub
 
--- ub_simulator/test.py
+++ ub_simulator/test.py
@@ -1,21 +1,38 @@
-import numpy
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+pi = np.pi
 from time import time
-import numba
+
+from qutip.qip.device import Processor
+from qutip.operators import sigmaz, sigmay, sigmax, destroy
+from qutip.states import basis
+from qutip.metrics import fidelity
+from qutip.qip.operations import rx, ry, rz, hadamard_transform
+from qutip.qip.circuit import qubit_states
+
+processor = Processor(N=1)
+state = qubit_states()
+processor.add_control(sigmay(), targets=0, label="sigmay")
+processor.add_control(sigmaz(), targets=0, label="sigmaz")
+'''for pulse in processor.pulses:
+    pulse.print_info()'''
 
 
-'''new_state = np.random.rand(2**qubits).reshape((2,)*qubits)
-new_state /= np.linalg.norm(new_state)
-q1 = QC1(qubits)
-q1.H(0)
-q1.X(2)
-print(q1.get_state())
-print('\n\n')
-q2 = QC2(qubits)
-q2.H(0)
-q2.X(2)
-print(q2.get_state())
-print('\n\n')
-q3 = QC3(qubits)
-q3.H(0)
-q3.X(2)
-print(q3.get_state())'''
+
+processor.pulses[0].coeff = np.array([.5])
+processor.pulses[0].tlist = np.array([0., pi])
+start = time()
+state = processor.run_state(init_state=state)
+for i in range(100):
+    processor.run_state(init_state=state.states[-1])
+end = time()
+print(end - start)
+
+start = time()
+state = qubit_states()
+processor.pulses[0].coeff = np.ones(100) * 0.5
+processor.pulses[0].tlist = np.arange(0, 100*np.pi, np.pi) # Esto es MUCHÍSIMO MÁS RÁPIDO
+processor.run_state(init_state=state)
+end = time()
+print(end - start)
